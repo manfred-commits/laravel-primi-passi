c1349643f55f2a5988e5cc38e95cbb1c4ff0108e
manfred-commits <manfredipiraino@gmail.com>
2021-11-04 14:49:41 +0100

    added style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,67 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Laravel-primi-passi</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+            .full-height {
+                height: 100vh;
+            }
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+            .position-ref {
+                position: relative;
+            }
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+            .content {
+                text-align: center;
+            }
+            .title {
+                font-size: 84px;
+            }
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 28px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+            li{
+                list-style: none;
+            }
+            li:hover a{
+                color: lightgray;
+            }
+        </style>
     </head>
     <body>
         
-        <nav>
+        <nav class="full-height flex-center">
             <ul>
                 @foreach ($nav as $element)
-                <li>                    
+                <li class="links flex-center">                    
                     <a href="{{$element['link']}}">
                         {{$element['title']}}
                     </a>
